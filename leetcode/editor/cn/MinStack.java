103ac9771a742b6e25350f5a639835e2e32a5083
zynie <2598049398@qq.com>
2020-10-28 17:46:22 +0800

    【20-10-28新增】

--- leetcode/editor/cn/MinStack.java
+++ leetcode/editor/cn/MinStack.java
@@ -1,15 +1,15 @@
-//设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。 
+//设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
 //
-// 
-// push(x) —— 将元素 x 推入栈中。 
-// pop() —— 删除栈顶的元素。 
-// top() —— 获取栈顶元素。 
-// getMin() —— 检索栈中的最小元素。 
-// 
 //
-// 
+// push(x) —— 将元素 x 推入栈中。
+// pop() —— 删除栈顶的元素。
+// top() —— 获取栈顶元素。
+// getMin() —— 检索栈中的最小元素。
 //
-// 示例: 
+//
+//
+//
+// 示例:
 //
 // 输入：
 //["MinStack","push","push","push","getMin","pop","top","getMin"]
@@ -27,56 +27,53 @@
 //minStack.pop();
 //minStack.top();      --> 返回 0.
 //minStack.getMin();   --> 返回 -2.
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// pop、top 和 getMin 操作总是在 非空栈 上调用。 
-// 
-// Related Topics 栈 设计 
+//
+// 提示：
+//
+//
+// pop、top 和 getMin 操作总是在 非空栈 上调用。
+//
+// Related Topics 栈 设计
 // 👍 706 👎 0
 
 package com.nzy.leetcode;
-public class MinStack{
-    public static void main(String[] args) {
-        Solution solution = new MinStack().new Solution();
-    }
+public static void main(String[] args) {}
 //leetcode submit region begin(Prohibit modification and deletion)
 class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
 
     /** initialize your data structure here. */
     public MinStack() {
-
+        stack=new Stack<>();
+        minStack=new Stack<>();
     }
-    
+
     public void push(int x) {
-
+        stack.push(x);
+        if (!minStack.empty()){
+            if (x<minStack.peek())minStack.push(x);
+        }else {
+            minStack.push(x);
+        }
     }
-    
+
     public void pop() {
-
+        Integer pop = stack.pop();
+        if (pop==minStack.peek())minStack.pop();
     }
-    
+
     public int top() {
-
+        return stack.peek();
     }
-    
-    public int getMin() {
 
+    public int getMin() {
+        return minStack.peek();
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(x);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
